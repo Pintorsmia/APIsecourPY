2922bfabfcdf8d40bae2743a816be64cfaa7cf4f
Janin Damien <damien.janin@soprasteria.com>
2020-04-07 14:04:36 +0200

    m

--- app.py
+++ app.py
@@ -14,25 +14,14 @@ api = Api(app)
 def index():
     return render_template('index.html')
 
-
- def week(i):
-        switcher={
-                0:'Sunday',
-                1:'Monday',
-                2:'Tuesday',
-                3:'Wednesday',
-                4:'Thursday',
-                5:'Friday',
-                6:'Saturday'
-             }
-         return switcher.get(i,"Invalid day of week")
-
 class CalculerPrix(Resource):
-     def prixdevise(self,devise,prix):
+    def get(self,distance,devise):
+        PRIXKILOMETRE = 5
         dollar = 1.1
         francsuisse = 1.06
         bitcoin = 0.00012
-        if devise == "euro":
+        prixtmp = float(distance) * PRIXKILOMETRE
+         if devise == "euro":
             retour = prix
             pass
         elif devise == "dollar":
@@ -48,13 +37,7 @@ class CalculerPrix(Resource):
             retour = "Erreur devise inconnue"
             pass
         return retour
-
-    def get(self,distance,monnaie):
-        PRIXKILOMETRE = 5
-        #TODO devise
-        prixtmp = float(distance) * PRIXKILOMETRE
-        return prixdevise(monnaie,prixtmp)
         #return prixdevise(monnaie,prixtmp) + " " + monnaie
 
-api.add_resource(CalculerPrix,'/API/calcul/<int:distance>/<string:monnaie>', endpoint = "calculPrix")
+api.add_resource(CalculerPrix,'/API/calcul/<int:distance>/<string:devise>', endpoint = "calculPrix")
 
