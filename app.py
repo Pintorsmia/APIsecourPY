ad400ecf3f8e04abdf30fbf31b302ea215007529
Janin Damien <damien.janin@soprasteria.com>
2020-04-07 13:10:32 +0200

    Test

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ TOKEN = 'cb48489b-567a-4458-8525-517390fb1220'
 
 app = Flask(__name__)
 api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('distance')
-parser.add_argument('monnaie')
+
 
 PRIXKILOMETRE = 0.25
 
@@ -18,10 +16,10 @@ def index():
     return render_template('index.html')
 
 class CalculerPrix(Resource):
-    def get(self):
+    def get(self,distance,monnaie):
         #TODO devise
-        args = parser.parse_args()
         prix = float(args['distance']) * PRIXKILOMETRE
         return {'prix': prix}
 
-api.add_resource(CalculerPrix, '/calculerPrix')
+api.add_resource(CalculerPrix,'/API/calcul/<int:distance>/<string:monnaie>', endpoint = "calculPrix")
+
